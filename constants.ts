7d063d2ed0ca036521fb819986989c4f5b0d220d
Onur Tosuner <128583602+azazel1805@users.noreply.github.com>
2025-09-22 15:15:09 +0300

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -5,7 +5,7 @@ export const DIFFICULTY_LEVELS = ["Easy", "Intermediate", "Advanced", "Expert"] 
 
 export const QUESTION_TYPES = {
     "Kelime Sorusu": "Kelime Sorusu",
-    "Dil Bilgisi Sorusu": "Dil Bilgisi Sorusu",
+    "Gramer Sorusu": "Gramer Sorusu",
     "Cloze Test Sorusu": "Cloze Test Sorusu",
     "Cümle Tamamlama Sorusu": "Cümle Tamamlama Sorusu",
     "Çeviri Sorusu": "Çeviri Sorusu",
